05d8b918c04bb97db0b73996405f97123b8673c6
3s-joaolopes <joao.lopes@threesigma.xyz>
2023-03-14 16:34:39 +0000

    clean-up

--- test/ProxyTest.t.sol
+++ test/ProxyTest.t.sol
@@ -45,16 +45,12 @@ contract ProxyTest is Test, VaultFixture {
     }
 
     function testProxy_Deposit() external {
-        uint256 monthsLocked;
-        uint256 hint;
-        uint256[] memory depositIds;
-
         vm.warp(time);
 
         // Alice deposits all her LPtokens for 6 months
         vm.startPrank(alice);
-        monthsLocked = 6;
-        hint = vault.getInsertPosition(block.timestamp + monthsLocked * SECONDS_IN_30_DAYS);
+        uint256 monthsLocked = 6;
+        uint256 hint = vault.getInsertPosition(block.timestamp + monthsLocked * SECONDS_IN_30_DAYS);
         LPtoken.approve(address(vault), ALICE_INITIAL_LP_BALANCE);
         vault.deposit(ALICE_INITIAL_LP_BALANCE, monthsLocked, hint);
         require(LPtoken.balanceOf(alice) == 0, "Failed to assert alice balance after deposit");
@@ -64,7 +60,7 @@ contract ProxyTest is Test, VaultFixture {
 
         // Alice withdraws her deposit and claims her rewards
         vault.withdraw();
-        depositIds = vault.getDepositIds(alice);
+        uint256[] memory depositIds = vault.getDepositIds(alice);
         vault.claimRewards(depositIds);
         uint256 expectedValue = 317 * SECONDS_IN_30_DAYS * 6;
         require(similar(rewardToken.balanceOf(alice), expectedValue), "Incorrect rewards");
