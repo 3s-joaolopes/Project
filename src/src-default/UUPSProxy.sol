1b2aa5b6ac6feabc186bb1eb8aae5068813785e0
3s-joaolopes <joao.lopes@threesigma.xyz>
2023-03-14 11:46:52 +0000

    added proy template

--- src/src-default/UUPSProxy.sol
+++ src/src-default/UUPSProxy.sol
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.0;
 
-contract UUPSProxy { }
+import "@openzeppelin/proxy/ERC1967/ERC1967Proxy.sol";
+
+contract UUPSProxy is ERC1967Proxy{
+    constructor (address _implementation, bytes memory _data) ERC1967Proxy (_implementation, _data){}
+}
