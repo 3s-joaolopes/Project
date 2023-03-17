f3ae10b68b9af4295ada3783631c116e3fc40ebb
3s-joaolopes <joao.lopes@threesigma.xyz>
2023-03-17 14:05:53 +0000

    initial commit

---
+++ src/src-default/storage/VaultStorage.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.0;
+
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+//import { Token } from "./Token.sol";
+import { OFToken } from "../OFToken.sol";
+import { IVault } from "../interfaces/IVault.sol";
+
+contract VaultStorage {
+
+    uint128 internal _totalShares;
+    uint128 internal _lastRewardsPerShare;
+    uint64 internal _lastRewardUpdateTime;
+    uint64 internal _idCounter;
+    address internal _owner;
+    bool internal _initialized;
+
+    mapping(address => uint128) internal _withdrawableAssets;
+    mapping(address => int128) internal _pendingRewards; //can be negative
+    mapping(uint64 => IVault.Deposit) internal _depositList;
+
+    IERC20 public asset;
+    OFToken public rewardToken;
+}
---
+++ src/src-default/storage/VaultV2Storage.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.0;
+
+import { VaultStorage } from "./VaultStorage.sol";
+import { ILayerZeroEndpoint } from "@layerZero/interfaces/ILayerZeroEndpoint.sol";
+
+contract VaultV2Storage is VaultStorage {
+
+    mapping(uint16 => bytes) internal _trustedRemoteLookup;
+    mapping(uint16 => uint16) internal _chainIdList;
+
+    ILayerZeroEndpoint internal _lzEndpoint;
+}
