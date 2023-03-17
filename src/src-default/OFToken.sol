cf1f5541a698508000a93cb30bc71f6f9a02431f
3s-joaolopes <joao.lopes@threesigma.xyz>
2023-03-17 16:20:00 +0000

    clean-up

--- src/src-default/OFToken.sol
+++ src/src-default/OFToken.sol
@@ -1,14 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import { IERC165 } from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
-import { IOFT } from "@layerZero/token/oft/IOFT.sol";
-import { OFTCore } from "@layerZero/token/oft/OFTCore.sol";
+import { OFT } from "@layerZero/token/oft/OFT.sol";
 
-// override decimal() function is needed
-contract OFToken is OFTCore, ERC20, IOFT {
+contract OFToken is OFT {
     address private _owner;
 
     error UnauthorizedError();
@@ -18,9 +13,8 @@ contract OFToken is OFTCore, ERC20, IOFT {
         _;
     }
 
-    constructor(address owner_, string memory _name, string memory _symbol, address _lzEndpoint)
-        ERC20(_name, _symbol)
-        OFTCore(_lzEndpoint)
+    constructor(address owner_, string memory name_, string memory symbol_, address lzEndpoint_)
+        OFT(name_, symbol_, lzEndpoint_)
     {
         _owner = owner_;
     }
@@ -30,35 +24,5 @@ contract OFToken is OFTCore, ERC20, IOFT {
     /// @param amount_   Amount of tokens to mint
     function mintRewards(address receiver_, uint256 amount_) external onlyOwner_ {
         _mint(receiver_, amount_);
-    }
-
-    function supportsInterface(bytes4 interfaceId) public view virtual override(OFTCore, IERC165) returns (bool) {
-        return interfaceId == type(IOFT).interfaceId || interfaceId == type(IERC20).interfaceId
-            || super.supportsInterface(interfaceId);
-    }
-
-    function token() public view virtual override returns (address) {
-        return address(this);
-    }
-
-    function circulatingSupply() public view virtual override returns (uint256) {
-        return totalSupply();
-    }
-
-    function _debitFrom(address _from, uint16, bytes memory, uint256 _amount)
-        internal
-        virtual
-        override
-        returns (uint256)
-    {
-        address spender = _msgSender();
-        if (_from != spender) _spendAllowance(_from, spender, _amount);
-        _burn(_from, _amount);
-        return _amount;
-    }
-
-    function _creditTo(uint16, address _toAddress, uint256 _amount) internal virtual override returns (uint256) {
-        _mint(_toAddress, _amount);
-        return _amount;
     }
 }
