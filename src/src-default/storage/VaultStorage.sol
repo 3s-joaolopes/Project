e6d277a823a737a2e55ed29f7ccc8c5284fe8d17
3s-joaolopes <joao.lopes@threesigma.xyz>
2023-03-17 15:59:08 +0000

    added comments

--- src/src-default/storage/VaultStorage.sol
+++ src/src-default/storage/VaultStorage.sol
@@ -6,17 +6,17 @@ import { OFToken } from "../OFToken.sol";
 import { IVault } from "../interfaces/IVault.sol";
 
 contract VaultStorage {
-    uint128 internal _totalShares;
-    uint128 internal _lastRewardsPerShare;
-    uint64 internal _lastRewardUpdateTime;
-    uint64 internal _idCounter;
-    address internal _owner;
-    bool internal _initialized;
+    uint128 internal _totalShares; // The total number of outstanding vault shares
+    uint128 internal _lastRewardsPerShare; // The amount of rewards per share on the last list update
+    uint64 internal _lastRewardUpdateTime; // The unix time of the last deposit list update
+    uint64 internal _idCounter; // The internal counter the generate a nonce for every deposit
+    address internal _owner; // The address of the owner of the vault
+    bool internal _initialized; // The status of the contract
 
-    mapping(address => uint128) internal _withdrawableAssets;
-    mapping(address => int128) internal _pendingRewards; //can be negative
-    mapping(uint64 => IVault.Deposit) internal _depositList;
+    mapping(address => uint128) internal _withdrawableAssets; // The amount of asset a depositor can withdraw
+    mapping(address => int128) internal _pendingRewards; // The amount of rewards a depositor can claim. It can be negative as it decreases everytime rewards are claimed but only increases when the deposit expires
+    mapping(uint64 => IVault.Deposit) internal _depositList; // The sorted list of deposits
 
-    IERC20 public asset;
-    OFToken public rewardToken;
+    IERC20 public asset; // The ERC20 token to use as the asset for deposits
+    OFToken public rewardToken; // The ERC20 token used to distribute rewards
 }
