de353dbafd0dee0a0c9256d5c0ab9004b81acc4e
3s-joaolopes <joao.lopes@threesigma.xyz>
2023-03-21 17:54:20 +0000

    added multi chain test

--- test/fuzz/MultiChainOperations.t.sol
+++ test/fuzz/MultiChainOperations.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 import "@forge-std/Test.sol";
 import { UUPSProxy } from "src/src-default/UUPSProxy.sol";
 import { VaultFixture } from "./../utils/VaultFixture.sol";
+import { LayerZeroHelper } from "./../utils/LayerZeroHelper.sol";
 import { IVault } from "src/src-default/interfaces/IVault.sol";
 import { IVaultV2 } from "src/src-default/interfaces/IVaultV2.sol";
 import { Vault } from "src/src-default/Vault.sol";
@@ -11,8 +12,35 @@ import { VaultV2 } from "src/src-default/VaultV2.sol";
 import { OFToken } from "src/src-default/OFToken.sol";
 import { LZEndpointMock } from "@layerZero/mocks/LZEndpointMock.sol";
 
-contract DepositFuzzTests is Test, VaultFixture {
+contract MultiChainOperationsFuzzTests is Test, LayerZeroHelper {
+    uint256 numberOfChains;
+
     function setUp() public override {
         super.setUp();
+    }
+
+    function _testFuzz_LayerZeroFunctionality(uint16[] memory chainIds_) external {
+        //Check for repeated entries
+        numberOfChains = chainIds_.length;
+        for (uint256 i = 0; i < numberOfChains; i++) {
+            vm.assume(chainIds_[i] != 998);
+        }
+        uint128 deposit_ = 1 ether;
+        address depositor_ = alice;
+        uint64 monthsLocked_ = 12;
+        uint64 randomHint_ = 3;
+        giveLPtokens(depositor_, numberOfChains * deposit_);
+
+        (address[] memory vaultsv2_, address[] memory endpoints_, address[] memory rewardTokens_) =
+            deployBatchOnChain(chainIds_);
+        connectVaults(chainIds_, vaultsv2_, endpoints_);
+
+        // Make deposits
+        for (uint256 i = 0; i < numberOfChains; i++) {
+            vm.startPrank(depositor_);
+            LPtoken.approve(vaultsv2_[i], uint256(deposit_));
+            IVaultV2(vaultsv2_[i]).deposit(deposit_, monthsLocked_, randomHint_);
+            vm.stopPrank();
+        }
     }
 }
