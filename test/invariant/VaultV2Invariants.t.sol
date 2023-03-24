d526a08b3cdc5112439b1fd3e6a525f3de45c71f
3s-joaolopes <joao.lopes@threesigma.xyz>
2023-03-24 16:36:41 +0000

    added artifacts and more invariants

--- test/invariant/VaultV2Invariants.t.sol
+++ test/invariant/VaultV2Invariants.t.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import { Test } from "@forge-std/Test.sol";
 import { Lib } from "test/utils/Library.sol";
 import { VaultV2Handler } from "./VaultV2Handler.sol";
-import { console } from "@forge-std/console.sol";
+import { console2 } from "@forge-std/console2.sol";
 
 contract VaultV2Invariants is Test {
     VaultV2Handler handler;
@@ -12,14 +12,32 @@ contract VaultV2Invariants is Test {
     function setUp() public {
         handler = new VaultV2Handler();
 
-        bytes4[] memory selectors = new bytes4[](1);
+        bytes4[] memory selectors = new bytes4[](4);
         selectors[0] = VaultV2Handler.deposit.selector;
-        //selectors[1] = VaultV2Handler.withdraw.selector;
-        //selectors[2] = VaultV2Handler.claimRewards.selector;
-        //selectors[3] = VaultV2Handler.skipTime.selector;
+        selectors[1] = VaultV2Handler.withdraw.selector;
+        selectors[2] = VaultV2Handler.claimRewards.selector;
+        selectors[3] = VaultV2Handler.skipTime.selector;
 
         targetSelector(FuzzSelector({ addr: address(handler), selectors: selectors }));
         targetContract(address(handler));
+        excludeArtifact("VaultV2");
+        excludeArtifact("OFToken");
+        excludeArtifact("LZEndpointMock");
+    }
+
+    // Assert that vaults have only been deployed once
+    function invariant_onlyDeployedOnce() public view {
+        assert(handler.numberOfDeployments() < 2);
+    }
+
+    // Assert that actors have been successfully deployed on all chains
+    function invariant_successfullyDeployedActors() public view {
+        uint256 numberOfChains_ = handler.getNumberOfChains();
+        bool deployedActors_;
+        for (uint256 i_ = 0; i_ < numberOfChains_; i_++) {
+            if (handler.getNumberOfDepositorsOnChain(i_) > 0) deployedActors_ = true;
+        }
+        if (numberOfChains_ > 0) assert(deployedActors_);
     }
 
     // Assert that all the vaults (on all the chains) have the same number of shares
@@ -27,7 +45,7 @@ contract VaultV2Invariants is Test {
         uint256 numberOfChains_ = handler.getNumberOfChains();
         uint256 firstVaultshares_;
         for (uint256 i_ = 0; i_ < numberOfChains_; i_++) {
-            uint256 vaultShares_ = handler.getVaultSharesByChainIndex(i_);
+            uint256 vaultShares_ = handler.getVaultSharesByIndex(i_);
             if (i_ == 0) firstVaultshares_ = vaultShares_;
             else assert(vaultShares_ == firstVaultshares_);
         }
@@ -35,20 +53,45 @@ contract VaultV2Invariants is Test {
 
     // Assert that the shares on the first vault match the expected shares
     function invariant_correctShares() public view {
-        assert(handler.getVaultExpectedShares() == handler.getVaultSharesByChainIndex(0));
-    }
-
-    // Assert that the balance of all vaults matches the amount of un-withdrawn asset
-    function invariant_correctAsset() public view {
-        uint256 numberOfChains_ = handler.getNumberOfChains();
-        for (uint256 i_ = 0; i_ < numberOfChains_; i_++) {
-            assert(handler.getVaultAssetBalanceByChainIndex(i_) == handler.getVaultUnwithdrawnAssetByChainIndex(i_));
+        if (handler.getNumberOfChains() > 0) {
+            assert(handler.getVaultExpectedShares() == handler.getVaultSharesByIndex(0));
         }
     }
 
-    // Assert that the asset balance of every vault matches the locked/un-withdrawn deposits
-    function invariant_VaultRewards() public {
-        console.log("Number of chains: ", handler.getNumberOfChains());
+    // Assert that balance of the vaults + depositors is the initial asset amount
+    function invariant_conservationOfAsset() public {
+        uint256 numberOfChains_ = handler.getNumberOfChains();
+        for (uint256 i_ = 0; i_ < numberOfChains_; i_++) {
+            uint256[] memory depositorAsset_ = handler.getDepositorsAssetByChainIndex(i_);
+            uint256 sumDepositorAsset_ = Lib.sumOfElements(depositorAsset_);
+            uint256 initialAsset_ = handler.getInitialAssetByChainIndex(i_);
+            uint256 vaultAsset_ = handler.getVaultAssetBalanceByIndex(i_);
+            assert(vaultAsset_ + sumDepositorAsset_ == initialAsset_);
+        }
+    }
+
+    // Assert that the balance of all vaults matches the amount of expected un-withdrawn asset
+    function invariant_correctAsset() public view {
+        uint256 numberOfChains_ = handler.getNumberOfChains();
+        for (uint256 i_ = 0; i_ < numberOfChains_; i_++) {
+            assert(handler.getVaultAssetBalanceByIndex(i_) == handler.getVaultExpectedUnwithdrawnAssetByIndex(i_));
+        }
+    }
+
+    // Log results
+    function invariant_LogSummary() public {
+        console2.log("Logs-------------");
+        uint256 numberOfChains_ = handler.getNumberOfChains();
+        for (uint256 i_ = 0; i_ < numberOfChains_; i_++) {
+            console2.log("Chain: ", i_ + 1, "/", handler.getNumberOfChains());
+            console2.log("Vault asset", handler.getVaultAssetBalanceByIndex(i_));
+            console2.log("Withdrawn asset", handler.getWithdrawnAssetByChainIndex(i_));
+        }
         //vm.writeLine("test/invariant/out.txt", vm.toString(handler.getNumberOfChains()));
+    }
+
+    // Ask handler to log results
+    function invariant_HandlerLogSummary() public {
+        handler.handlerLog();
     }
 }
